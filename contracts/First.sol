3551b22f70448162914a15ccde9ccebe29ff06e5
Moneshsoni <sonimonesh115@gmail.com>
2022-07-14 10:18:03 +0530

    successfully update

--- contracts/First.sol
+++ contracts/First.sol
@@ -1,13 +1,30 @@
-pragma solidity 0.8.0;
+pragma solidity ^0.4.21;
 
-contract SimpleStorage {
-    uint storedData;
+contract Coin {
+    // The keyword "public" makes those variables
+    // readable from outside.
+    address public minter;
+    mapping (address => uint) public balances;
 
-    function set(uint x) public {
-        storedData = x;
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Sent(address from, address to, uint amount);
+
+    // This is the constructor whose code is
+    // run only when the contract is created.
+    function Coin() public {
+        minter = msg.sender;
     }
 
-    function get() public view returns (uint) {
-        return storedData;
+    function mint(address receiver, uint amount) public {
+        if (msg.sender != minter) return;
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
     }
 }
