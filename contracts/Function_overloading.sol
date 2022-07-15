967378397cc4e2a1cd7204e50516ca642523c4b3
Moneshsoni <sonimonesh115@gmail.com>
2022-07-15 10:18:42 +0530

    function overloading

---
+++ contracts/Function_overloading.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+contract Fun_Over{
+    function Fun_ove(uint x) public pure returns(uint){
+        return x;
+    }
+    function Fun_ove(uint x, uint y) public pure returns(uint){
+        return x+y;
+    } 
+}
